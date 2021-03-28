b3de8fc6087e7e5248258b8944677198f637b531
ares <server.ares@gmail.com>
2021-03-28 20:08:08 +0300

    v1.0.1

--- main/views.py
+++ main/views.py
@@ -6,11 +6,16 @@ from main.models import Categories, Advertise, Settings
 def index(request, path):
     issue = int(Settings.objects.filter(id=1).values('svalue')[0]['svalue'])
     categories = list(Categories.objects.all().values())
+    if 'en' in path:
+        lang = 'en'
+    else:
+        lang = 'ru'
     classys = list(Advertise.objects.filter(active=1, advertise_template_id__in=[1, 2, 7], end_issue__gte=issue).order_by(
             '-id').values())
     base_context = {
         'categories': categories,
-        'classys': classys
+        'classys': classys,
+        'lang': lang
     }
     context = {}
 
